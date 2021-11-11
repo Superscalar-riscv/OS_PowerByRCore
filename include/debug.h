6436cdad18f3352dd7e88b386ebf17486b7d5260
szu_yy <814133240@qq.com>
2021-11-12 00:56:49 +0800

    update debug.h

--- include/debug.h
+++ include/debug.h
@@ -1,18 +1,19 @@
 
 #include "printf.h"
 #include "sbi.h"
+#include "common.h"
 
-
-#define DEBUG(...) \
+#define LOG(...) \
   do { \
     printf(__VA_ARGS__); \
   } while (0)
   
-
+#define DEBUG(fmt, ...) LOG("\33[1;34m[%s,%d,%s] " format "\33[0m\n", \
+        __FILE__, __LINE__, __func__, ## __VA_ARGS__)
 
 #define panic(fmt, ...) \ 
   do { \
-    DEBUG("\33[1;34m Panicked at [%s,%d,%s] " fmt "\33[0m\n", __FILE__, __LINE__, __func__, ## __VA_ARGS__); \
+    LOG(ANSI_LIGHT_BLUE"Panicked at [%s,%d,%s] " fmt ANSI_NONE"\n", __FILE__, __LINE__, __func__, ## __VA_ARGS__); \
     shutdown(); \
   } while (0)
   
